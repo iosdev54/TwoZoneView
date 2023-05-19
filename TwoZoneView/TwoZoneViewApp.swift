e69f6123cb8fd2dff3e394f4fdfff1dbcfe2d039
iosdev54 <iosdev54@gmail.com>
2023-05-19 12:53:42 +0300

    Initial Commit

---
+++ TwoZoneView/TwoZoneViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoZoneViewApp.swift
+//  TwoZoneView
+//
+//  Created by Dmytro Grytsenko on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwoZoneViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
